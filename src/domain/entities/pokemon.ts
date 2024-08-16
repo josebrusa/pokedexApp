105e5db0b4121b31c7e2d1006e37a3b945ad7ff7
josebrusa <josebrusa@outlook.com>
2024-08-16 02:23:45 +0200

    finalizacion de la seccion pokedex

--- src/domain/entities/pokemon.ts
+++ src/domain/entities/pokemon.ts
@@ -9,6 +9,22 @@ export interface Pokemon {
 
     color: string;
 
+    games: string[];
+    stats: Stat[];
+    abilities: string[];
+    moves: Move[];
+
+}
+
+export interface Stat {
+
+    name: string;
+    value: number;
+}
+
+export interface Move {
+    name: string;
+    level: number;
 }
 
 
